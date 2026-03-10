19817d1cccf2557860d965ca03c05c9882c2c168
elennvtt <135832340+elennvtt@users.noreply.github.com>
2026-03-10 18:29:23 +0000

    terceiro commit

---
+++ sandbox/00_rascunhos/rascunho02/draft.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var num1, num2 float64 //variavel 
+    fmt.Scan(&num1, &num2) //lendo e escrevendo em (variaveis)
+	fmt.Println(num1+num2) //imprimir
+    fmt.Println(num1-num2)
+    
+}
