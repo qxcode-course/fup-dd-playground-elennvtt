738c48959ea3f4411d831df37fb5aabd4ccd8b0b
elennvtt <135832340+elennvtt@users.noreply.github.com>
2026-03-30 18:25:32 +0000

    quinto commit

---
+++ fup/segundos/go/draft.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tempo int
+	//entrada
+	fmt.Scan(&tempo)
+	//saida
+
+}
---
+++ fup/sobrou/go/draft.go
@@ -0,0 +1,30 @@
+package main
+import "fmt"
+func main() {
+    var p1, p2, p3 float64
+    var v1, v2, v3 float64
+    var din float64
+   
+   //entrada
+   //quantidade
+   
+   fmt.Scan(&p2)
+   fmt.Scan(&p3)
+   // valores
+   fmt.Scan(&v1)
+   fmt.Scan(&v2)
+   fmt.Scan(&v3)
+   fmt.Scan(&din)
+
+   //calculo
+   v1 = v1 * p1
+   v2 = v2 * p2
+   v3 = v3 * p3
+   //saida 
+ var troco float64 = din - (v1 + v2 + v3)
+   fmt.Printf("%.2f\n", troco)
+}
+
+
+
+
