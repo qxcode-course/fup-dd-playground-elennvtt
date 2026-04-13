25ffdc70e0b31d7efd39229499fd1959aa10bd57
elennvtt <135832340+elennvtt@users.noreply.github.com>
2026-04-13 17:50:44 +0000

    teste de nivel

---
+++ eval1/bala/src/go/draft.go
@@ -0,0 +1,19 @@
+package main
+import (
+    "fmt"
+    "math"
+)
+func main() {
+    var x1, x2, y1, y2 float64
+
+    fmt.Scan(&x1)
+    fmt.Scan(&y1)
+    fmt.Scan(&x2)
+    fmt.Scan(&y2)
+    
+
+    distancia := math.Sqrt(math.Pow(x2-x1,2)+ math.Pow(y2-y1,2))
+
+    fmt.Printf("%.2f\n", distancia)
+
+}
---
+++ eval1/mumia/src/go/draft.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+    var nome string
+    var idade int
+
+    if idade < 12 {
+        
+    }
+}
